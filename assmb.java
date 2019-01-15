6e8dae62f6fe42a38ed0300db74d732ef676f7e6
dsathe <41478839+dsathe@users.noreply.github.com>
2019-01-15 04:16:11 +0000

    Add files via upload

---
+++ assmb.java
@@ -0,0 +1,11 @@
+package assembler;
+
+import java.io.IOException;
+
+public class assmb {
+	public static void main(String args[]) throws IOException
+	{
+		pass1 p1=new pass1();
+		p1.genint();
+	}
+}
