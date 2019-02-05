3422af0a55ee8b6c814f6827f3f32ac0a33cbb3e
dsathe <41478839+dsathe@users.noreply.github.com>
2019-02-05 09:42:28 +0530

    Update macro.java

--- macro.java
+++ macro.java
@@ -26,23 +26,26 @@ public class macro {
 		FileWriter fw=new FileWriter(new File("/home/TE/3167/spos/workspace/Macros/intermediate.asm"));
 		BufferedWriter out =new BufferedWriter(fw);
 		Boolean inmacro=false;
+		String arg[]=new String[4];
+		int argno=0;
 		while(true)
 		{
 			line=br.readLine();
 			//System.out.println(line);
 			if(line.contains("\tEND"))
 			{
-				//System.out.println("Hi checked");
+				System.out.println("MDT contents");
+				out.write(line);
 				for(int i=0;i<mdt.size();i++)
 				{
 					String s=mdt.get(i);
 					System.out.println(s);
 				}
-
+				System.out.println("MNT contents");
 				for(int i=0;i<mntid;i++)
 				{
 					nametable n=mnt.get(i);
-					System.out.println(n.getname()+"  "+n.getstartid());
+					System.out.println(n.getname()+"  "+n.nop+"  "+n.getstartid());
 				}
 				break;
 			}
@@ -60,9 +63,11 @@ public class macro {
 						count++;
 					}
 				}
+				argno=count;
 				nametable nt=new nametable(lp[2],count,mdt.size());
 				mnt.add(nt);
 				mntid++;
+				arg=lp[3].split(",");
 				System.out.println("Macro added !!!");
 				//System.out.println(nt.getname());
 				inmacro=true;
@@ -77,19 +82,87 @@ public class macro {
 			}
 			if(inmacro==true)
 			{
-				mdt.add(line);	
+				String line1=line;
+				String changed="";
+				int flag=1;
+				if(line1.contains("&"))
+				{
+					int pos=line1.indexOf("&");
+					String args=line1.substring(pos,pos+2);
+					//System.out.println(argno);
+					for(int i=0;i<argno;i++)
+					{
+						if(arg[i].equals(args))
+						{
+							String rep="#"+(i+1);
+							changed=line1.replace(args, rep);
+							flag=0;
+						}
+					}
+				}
+				if(flag==0)
+				{
+					mdt.add(changed);
+				}
+				else
+				{
+					mdt.add(line1);
+				}
 			}
 			else
 			{
-				out.write(line);
+				out.write(line+"\n");
 				out.flush();
 			}
 			
 			
 		}
 		out.close();
+		br.close();	
+	}
+	
+	
+	void pass2() throws IOException
+	{
+		BufferedReader br=new BufferedReader(new FileReader ("/home/TE/3167/spos/workspace/Macros/intermediate.asm"));
+		String line="";
+		FileWriter fw=new FileWriter(new File("/home/TE/3167/spos/workspace/Macros/macroexpan.asm"));
+		BufferedWriter out =new BufferedWriter(fw);
+		Boolean inmacro=false;
+		String arg[]=new String[4];
+		int argno=0;
+		while(true)
+		{
+			line=br.readLine();
+			String lp[]=new String[4];
+			lp=line.split("\t");
+			int flag=0;
+			if(line.contains("\tEND"))
+			{
+				out.write(line);
+				break;
+			}
+			for(int i=0;i<mnt.size();i++)
+			{
+				nametable n=mnt.get(i);
+				if(lp[1].contains(n.getname()))
+				{
+					arg=lp[2].split(",");
+					flag=1;
+					int ptr=n.getstartid();
+					while(true)
+					{
+						
+					}
+				}
+			}
+			if(flag==1)
+			{
+				out.write(line);
+			}
+		}
 		br.close();
-		
+		out.close();
 	}
 	
 	public static void main(String args[]) throws IOException
